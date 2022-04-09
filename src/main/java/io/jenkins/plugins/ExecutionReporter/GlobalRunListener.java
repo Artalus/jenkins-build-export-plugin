8ba6f058a788975ad43890611a91bc90a80c50b7
Artalus <>
2022-04-09 20:15:38 +0300

    prepare to pipeline

--- src/main/java/io/jenkins/plugins/ExecutionReporter/GlobalRunListener.java
+++ src/main/java/io/jenkins/plugins/ExecutionReporter/GlobalRunListener.java
@@ -6,6 +6,11 @@ import hudson.model.*;
 
 import java.util.logging.*;
 
+import org.jenkinsci.plugins.workflow.graphanalysis.DepthFirstScanner;
+import org.jenkinsci.plugins.workflow.actions.*;
+import org.jenkinsci.plugins.workflow.cps.nodes.*;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+
 @Extension
 public class GlobalRunListener extends RunListener<Run<?, ?>> {
     private static final Logger logger = Logger.getLogger(GlobalRunListener.class.getName());
@@ -14,5 +19,17 @@ public class GlobalRunListener extends RunListener<Run<?, ?>> {
     public void onCompleted(Run<?, ?> build, TaskListener listener) {
         listener.getLogger().println("[ExecutionReporter] println ggg");
         logger.log(Level.WARNING, "log ggg");
+
+        if (!(build instanceof WorkflowRun)) {
+            String msg = String.format("Ignoring build %s: not a WorkflowRun", build);
+            logger.log(Level.INFO, msg);
+            return;
+        }
+        traverse((WorkflowRun)build);
+    }
+
+    private void traverse(WorkflowRun b) {
+        String msg = String.format("[ExecutionReporter] Traversing build: %s", b);
+        logger.log(Level.INFO, msg);
     }
 }
