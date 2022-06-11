7b99fa211ad2034ab74d05f53ffcfe16555a78b9
Artalus <>
2022-06-11 12:29:47 +0300

    add buildResult field to json

--- src/main/java/artalus/plugins/buildexport/PostData.java
+++ src/main/java/artalus/plugins/buildexport/PostData.java
@@ -10,6 +10,7 @@ class PostData {
     public String buildName;
     public String buildFullName;
     public String buildUrl;
+    public String buildResult;
     public int buildNumber;
     public Map<String, String> buildParameters;
     public ArrayList<NodeData> nodes;
@@ -20,6 +21,8 @@ class PostData {
         this.buildFullName = run.getFullDisplayName();
         this.buildUrl = String.format("%s%s", Jenkins.get().getRootUrl(), run.getUrl());
         this.buildNumber = run.getNumber();
+        this.buildResult = run.getResult().toString();
+
         this.buildParameters = new HashMap<String, String>();
         ParametersAction p = run.getAction(ParametersAction.class);
         if (p != null) {
